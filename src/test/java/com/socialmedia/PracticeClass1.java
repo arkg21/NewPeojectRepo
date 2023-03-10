2405c7e314593100fe7dd6041a1ac61c3350a282
Archit <achugaju34@gmail.com>
2023-03-10 15:01:38 +0530

    new project

---
+++ src/test/java/com/socialmedia/PracticeClass1.java
@@ -0,0 +1,18 @@
+package com.socialmedia;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass1 
+{
+@Test
+public void practiceClass1()
+{
+	System.out.println("Test 1");
+}
+
+@Test
+public void practiceClass2()
+{
+	System.out.println("Test 2");
+}
+}
